b1090986b48d80c5da05641f8b7f0977a33be786
Retroflux <jlange@mail.uoguelph.ca>
2019-04-02 12:23:32 -0400

    Used to temporarily hold changes. Will be merged into finalVisitor

--- Checkpoint3/AssemblyCodeCreator.java
+++ Checkpoint3/AssemblyCodeCreator.java
@@ -46,10 +46,10 @@ public class AssemblyCodeCreator implements AbsynVisitor {
     public void visit(AssignExp exp, int level) {
         emitComment("-> op");
         exp.lhs.accept(this, 0);
-        emitRegisterMemory("ST", ac, offset--, fp, "op: push left");
+        emitRegisterMemory(" ST", ac, offset--, fp, "op: push left");
         exp.rhs.accept(this, level);
-        emitRegisterMemory("LD", 1, ++offset, fp, "op: load left");
-        emitRegisterMemory("ST", ac, ac, 1, "assign: store value");
+        emitRegisterMemory(" LD", 1, ++offset, fp, "op: load left");
+        emitRegisterMemory(" ST", ac, ac, 1, "assign: store value");
         emitComment("<- op");
     }
 
@@ -104,15 +104,15 @@ public class AssemblyCodeCreator implements AbsynVisitor {
         while (args != null) {
             args.head.accept(this, level);
             args = args.tail;
-            emitRegisterMemory("ST", ac, offset, fp, "store arg val"); // 57:     ST  0,-6(5) 	store arg val ???
+            emitRegisterMemory(" ST", ac, offset, fp, "store arg val"); // 57:     ST  0,-6(5) 	store arg val ???
         }
-        emitRegisterMemory("ST", fp, offset, fp, "push ofp");
+        emitRegisterMemory(" ST", fp, offset, fp, "push ofp");
         emitRegisterMemory("LDA", fp, offset, fp, "push frame");
         emitRegisterMemory("LDA", ac, 1, pc, "load ac with ret ptr");
         if (exp.func.equals("input")) emitRM_Abs("LDA", pc, 4, "jump to fun loc"); // doesn't work
         else if (exp.func.equals("output")) emitRM_Abs("LDA", pc, 7, "jump to fun loc");
         else emitRM_Abs("LDA", pc, entry, "jump to fun loc");
-        emitRegisterMemory("LD", fp, ac, fp, "pop frame");
+        emitRegisterMemory(" LD", fp, ac, fp, "pop frame");
         if (this.symTable.getLast().containsKey(exp.func)) {
             if (((SymItem) this.symTable.getLast().get(exp.func)).level == -1)
                 ((SymItem) this.symTable.getLast().get(exp.func)).level = -2; // mark as used prototype
@@ -135,7 +135,7 @@ public class AssemblyCodeCreator implements AbsynVisitor {
         emitBackup(savedLoc);
         emitRM_Abs("LDA", pc, savedLoc2, "while: absolute jmp to test");
         emitRestore();
-        emitRegisterMemory("JEQ", ac, emitLoc--, pc, "while: jmp to end");
+        emitRegisterMemory("JEQ", ac, emitLoc, pc, "while: jmp to end");
         emitComment("<- while");
 
 //        printMap(this.symTable.getFirst().entrySet().iterator(), level);
@@ -199,7 +199,7 @@ public class AssemblyCodeCreator implements AbsynVisitor {
                 //TODO store value
 
                 int tempLocation = emitSkip(1);
-                emitRegisterMemory("ST", fp, -1, fp, "store return");
+                emitRegisterMemory(" ST", ac, -1, fp, "store return");
                 entry = emitLoc-1;
 
                 currFunc = exp.func;
@@ -252,24 +252,24 @@ public class AssemblyCodeCreator implements AbsynVisitor {
 
     public void visit(DecList expList, int level) {
         emitComment("Standard prelude: ");
-        emitRegisterMemory("LD", gp, 0, ac, "load gp with maxaddress");
+        emitRegisterMemory(" LD", gp, 0, ac, "load gp with maxaddress");
         emitRegisterMemory("LDA", fp, 0, gp, "copy to gp to fp");
-        emitRegisterMemory("ST", 0, 0, 0, "clear location 0");
+        emitRegisterMemory(" ST", 0, 0, 0, "clear location 0");
         int savedLoc = emitSkip(1);
 
         /* Generate input function */
         emitComment("Jump around i/o routines here");
         emitComment("code for input routine");
-        emitRegisterMemory("ST", 0, -1, fp, "store return");
-        emitRegisterOnly("IN", 0, 0, 0, "input");
-        emitRegisterMemory("LD", pc, -1, fp, "return to caller");
+        emitRegisterMemory(" ST", 0, -1, fp, "store return");
+        emitRegisterOnly(" IN", 0, 0, 0, "input");
+        emitRegisterMemory(" LD", pc, -1, fp, "return to caller");
 
         /* Generate output function */
         emitComment("code for output routine");
-        emitRegisterMemory("ST", 0, -1, fp, "store return");
-        emitRegisterMemory("LD", 0, -2, fp, "load output value");
+        emitRegisterMemory(" ST", 0, -1, fp, "store return");
+        emitRegisterMemory(" LD", 0, -2, fp, "load output value");
         emitRegisterOnly("OUT", 0, 0, 0, "output");
-        emitRegisterMemory("LD", 7, -1, fp, "return to caller");
+        emitRegisterMemory(" LD", 7, -1, fp, "return to caller");
         int savedLoc2 = emitSkip(0);
 
         /* Set emitLoc to previously stored value
@@ -288,7 +288,7 @@ public class AssemblyCodeCreator implements AbsynVisitor {
         emitRegisterMemory("LDA", fp, globalOffset, fp, "push frame" );
         emitRegisterMemory("LDA", ac, 1, pc, "load ac with ret ptr" );
         emitRM_Abs("LDA", pc, entry, "jump to main loc" );
-        emitRegisterMemory("LD", fp, 0, fp, "pop frame" );
+        emitRegisterMemory(" LD", fp, 0, fp, "pop frame" );
         emitComment("End of execution.");
         emitRegisterOnly("HALT", 0, 0, 0, "" );
 
@@ -313,7 +313,7 @@ public class AssemblyCodeCreator implements AbsynVisitor {
         emitComment("-> id");
         emitComment("looking up id: " + exp.name);
         if (isParam == 0) emitRegisterMemory("LDA", ac, tempOffset, fp, "load id address");
-        else emitRegisterMemory("LD", ac, tempOffset, fp, "load id value");
+        else emitRegisterMemory(" LD", ac, tempOffset, fp, "load id value");
         emitComment("<- id");
     }
 
@@ -324,9 +324,9 @@ public class AssemblyCodeCreator implements AbsynVisitor {
     public void visit(OpExp exp, int level) {
         emitComment("-> op");
         exp.left.accept(this, level);
-        emitRegisterMemory("ST", ac, offset--, fp, "op: push left");
+        emitRegisterMemory(" ST", ac, offset--, fp, "op: push left");
         exp.right.accept(this, level);
-        emitRegisterMemory("LD", 1, ++offset, fp, "op: load left");
+        emitRegisterMemory(" LD", 1, ++offset, fp, "op: load left");
         switch (exp.op){
             case OpExp.PLUS:
                 emitRegisterOnly("ADD",ac,1,ac, "op +");
@@ -454,13 +454,25 @@ public class AssemblyCodeCreator implements AbsynVisitor {
 
     //Called emitRO in Fei's slides
     public void emitRegisterOnly(String operation, int regDestination, int val1, int  val2,  String comment) {
-        String generatedString = "\t" + emitLoc + ":\t" + operation + "\t" + regDestination + "," + val1 + "," + val2 + " \t" + comment + "\n";
+        String generatedString;
+        if (emitLoc < 10){
+            generatedString = "  ";
+        }else{
+            generatedString = " ";
+        }
+        generatedString = generatedString + emitLoc + ":    " + operation + "  " + regDestination + "," + val1 + "," + val2 + "  " + comment + "\n";
         writeToFile(generatedString);
         emitLoc++;
     }
     //Called emitRM in Fei's slides
     public void emitRegisterMemory(String operation, int regDestination, int offset, int val1, String comment) {
-        String generatedString = "\t" + emitLoc + ":\t" + operation + "\t" + regDestination + "," + offset + "(" + val1 + ") \t" + comment + "\n";
+        String generatedString;
+        if (emitLoc < 10){
+            generatedString = "  ";
+        }else{
+            generatedString = " ";
+        }
+        generatedString = generatedString + emitLoc + ":    " + operation + "  " + regDestination + "," + offset + "(" + val1 + ")  " + comment + "\n";
         writeToFile(generatedString);
         emitLoc++;
         if (highEmitLoc < emitLoc) highEmitLoc = emitLoc;
@@ -468,7 +480,13 @@ public class AssemblyCodeCreator implements AbsynVisitor {
 
     // taken from the lecture slides
     public void emitRM_Abs(String op, int r, int a, String c) {
-        String generatedString = "\t" + emitLoc + ":\t" + op + "\t" + r + "," + (a-(emitLoc+1)) + "(" + pc + ") \t";
+        String generatedString;
+        if (emitLoc < 10){
+            generatedString = "  ";
+        }else{
+            generatedString = " ";
+        }
+        generatedString = generatedString + emitLoc + ":    " + op + "  " + r + "," + (a-(emitLoc+1)) + "(" + pc + ")  ";
         writeToFile(generatedString);
         emitLoc++;
         if ( TraceCode == 1) writeToFile(c + "\n");
@@ -477,7 +495,7 @@ public class AssemblyCodeCreator implements AbsynVisitor {
     }
 
     // taken from the lecture slides
-    //calculates skip distance based on input, highEmitLoc, and the highEmitLoc
+    //calculates skip distance based on input, highEmitLoc, and the distanceGiven
     public int emitSkip (int distance) {
         int i = emitLoc;
         emitLoc += distance;
